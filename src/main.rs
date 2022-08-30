58eb613c7eec7dcacf0f0bcc7fd502129e8ef313
barlette <leobarlette@gmail.com>
2022-08-29 21:03:16 -0300

    Solved problem with hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,53 @@
 use std::collections::HashMap;
 
 fn main() {
-    // let nums: Vec<i32> = vec![2, 7, 11, 15];
-    // let target: i32 = 9;
+    let nums: Vec<i32> = vec![2, 7, 11, 15];
+    let target: i32 = 9;
 
     // let nums: Vec<i32> = vec![3, 2, 4];
     // let target: i32 = 6;
 
-    let nums: Vec<i32> = vec![3, 3];
-    let target: i32 = 6;
+    // let nums: Vec<i32> = vec![3, 3];
+    // let target: i32 = 6;
 
-    println!("{:?}", two_sum(nums, target))
+    //println!("{:?}", two_sum_first_version(nums, target))
+    println!("{:?}", two_sum_second_version(nums, target));
 }
 
-fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+fn two_sum_second_version(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut diffs = HashMap::new();
-    let mut output: Vec<i32> = vec![];
-
-    for x in 1..nums.len() {
-        diffs.insert(nums[x], target - nums[x]);
-
-        // let index = match nums
-        //     .iter()
-        //     .position(|&element| element == (target - nums[x]))
-        // {
-        //     Some(value) => value as i32,
-        //     None => -1 as i32,
-        // };
-
-        // if index > -1 && x != index as usize {
-        //     output.push(x as i32);
-        //     output.push(index);
-        //     return output;
-        // }
+    for x in 0..nums.len() {
+        let diff = target - nums[x];
+        if !diffs.get(&nums[x]).is_none() && (*diffs.get(&nums[x]).unwrap_or(&-1) != x as i32) {
+            return vec![*diffs.get(&nums[x]).unwrap(), x.try_into().unwrap()];
+        }
+        diffs.insert(diff, x as i32);
     }
-
-    return output;
+    return vec![];
 }
+
+
+// fn two_sum_first_version(nums: Vec<i32>, target: i32) -> Vec<i32> {
+//     let mut output: Vec<i32> = vec![];
+
+//     for x in 1..nums.len() {
+
+//         let index = match nums
+//             .iter()
+//             .position(|&element| element == (target - nums[x]))
+//         {
+//             Some(value) => value as i32,
+//             None => -1 as i32,
+//         };
+
+//         if index > -1 && x != index as usize {
+//             output.push(x as i32);
+//             output.push(index);
+//             return output;
+//         }
+//     }
+
+//     return output;
+// }
+
+
